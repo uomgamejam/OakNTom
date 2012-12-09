823e4477b13d803ffe04365e6dbac5d9c4a0b0f7
Tom Robinson <shadlington@thd.vg>
2012-12-09 03:49:11 +0000

    Scripty progress

---
+++ Brainomnom/Assets/Scripts/globals.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class globals : MonoBehaviour {
+
+	public static int playerTurns = 0;
+	
+	public static int goldTurns = 10;
+	
+	public enum teams { Zombies = 0, Humans = 1 }
+	public static int teamCount = 2;
+	public static teams teamTurn = 0;
+	
+	public static float tileWidth = 30;
+	public static float tileHeight = 30;
+	public static float tileDiag = Mathf.Sqrt(Mathf.Pow(tileWidth, 2) + Mathf.Pow(tileHeight, 2));
+	
+	public static float orthSize;
+	public static float orthSizeX;
+	public static float orthSizeY;
+	public static float camRatio;
+	
+	public enum team { None, Zombie, Human }
+	public static team activeTeam = None;
+	public static OTAnimatingSprite activePerson;
+	
+	public enum direction { Left, Right, Up, Down, TopLeft, TopRight, DownLeft, DownRight }
+
+	public void Start()
+	{
+		// gather information from the camera to find the screen size
+		globals.camRatio = 1.333f; // 4:3 is 1.333f (800x600) 
+		globals.orthSize = Camera.mainCamera.camera.orthographicSize;
+		globals.orthSizeX = globals.orthSize * globals.camRatio;
+	}
+
+	public void Update() 
+	{
+	}
+	
+	public void EndTurn()
+	{
+		teamTurn = (teamTurn + 1) % teamCount;
+	}
+}
