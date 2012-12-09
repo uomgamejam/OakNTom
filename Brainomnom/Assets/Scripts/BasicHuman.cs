959a194f884de5a0d52ff4c21fed0e967d8ad25e
Tom Robinson <shadlington@thd.vg>
2012-12-09 06:02:26 +0000

    Script tweaks

--- Brainomnom/Assets/Scripts/BasicHuman.cs
+++ Brainomnom/Assets/Scripts/BasicHuman.cs
@@ -6,6 +6,10 @@ public class BasicHuman: MonoBehaviour {
 	public enum basicHumanType { Still, Agg, Cow }
 	public basicHumanType type;
 	public bool isMoving = false;
+	public enum actionType { None, Turn, Move, Attack }
+	public actionType action = None;
+	public globals.direction currentDirection = globals.direction.Top;
+	public globals.direction newDirection = globals.direction.Top;
 	
 	private Transform thisTransform;
 	
--- Brainomnom/Assets/Scripts/BasicZombie.cs
+++ Brainomnom/Assets/Scripts/BasicZombie.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class BasicZombie: MonoBehaviour {
 
 	public bool isMoving = false;
+	public enum actionType { None, Turn, Move, Attack }
+	public actionType action = None;
+	public globals.direction currentDirection = globals.direction.Top;
+	public globals.direction newDirection = globals.direction.Top;
 	
 	private Transform thisTransform;
 	
